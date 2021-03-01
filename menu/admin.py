ecd4caec2d5479805ca2e4ea77a379015c2a5106
Pipicallu <thomas.vassallo@gmail.com>
2021-03-01 14:06:20 +0000

    customized admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 from .models import Ingredients, Category, DietaryRequirements, SidesAndDrinks
 
-admin.site.register(Ingredients)
-admin.site.register(Category)
-admin.site.register(DietaryRequirements)
-admin.site.register(SidesAndDrinks)
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'is_vegetarian',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class SidesAndDrinksAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'is_vegetarian',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class DietaryRequirementsAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Ingredients, IngredientsAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(DietaryRequirements, DietaryRequirementsAdmin)
+admin.site.register(SidesAndDrinks, SidesAndDrinksAdmin )
--- menu/models.py
+++ menu/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +16,9 @@ class Category(models.Model):
 
 
 class DietaryRequirements(models.Model):
+    class Meta:
+        verbose_name_plural = 'Dietary Requirements'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
@@ -25,6 +31,9 @@ class DietaryRequirements(models.Model):
 
 
 class SidesAndDrinks(models.Model):
+    class Meta:
+        verbose_name_plural = 'Sides and Drinks'
+
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254, null=True, blank=True)
@@ -37,11 +46,12 @@ class SidesAndDrinks(models.Model):
 
     def __str__(self):
         return self.name
-    
-     
 
 
 class Ingredients(models.Model):
+    class Meta:
+        verbose_name_plural = 'Ingredients'
+
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254, null=True, blank=True)
