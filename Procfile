d2ebb47474efeb627eeaed107bd10e38f46ea715
Pipicallu <thomas.vassallo@gmail.com>
2021-04-27 09:07:55 +0000

    in process of deploying app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SammichPatch.wsgi:application
--- SammichPatch/settings.py
+++ SammichPatch/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'gqk*-0s4z7x@l)upwlcv-vb@c3886c273$^hgwb&0_yopo^2sd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sammichpatch.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'SammichPatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
